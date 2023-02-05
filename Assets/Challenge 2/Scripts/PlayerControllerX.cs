bdc58512bd8e5de9f1ec5dcfaeb0a93740bac5ff
Nerubi <sm4391@stmartins.edu>
2023-02-05 18:57:33 +0100

    first commit

---
+++ Assets/Challenge 2/Scripts/PlayerControllerX.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerControllerX : MonoBehaviour
+{
+    public GameObject dogPrefab;
+    private bool canPress = true;
+
+    private void Start()
+    {
+        InvokeRepeating("TimePassed", 0, 1);
+    }
+
+
+    // Update is called once per frame
+    void Update()
+    {
+        // On spacebar press, send dog
+        if (Input.GetKeyDown(KeyCode.Space) && canPress == true)
+        {
+            Instantiate(dogPrefab, transform.position, dogPrefab.transform.rotation);
+            canPress = false;
+        }
+    }
+
+    void TimePassed()
+    {
+        // On spacebar press, send dog
+        canPress = true;
+    }
+
+}
---
+++ Assets/Scripts/PlayerControl.cs
@@ -0,0 +1,44 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerControl : MonoBehaviour
+{
+    public float horizontalInput;
+    public float playerSpeed = 10f;
+    public float xRange = 11.0f;
+
+    public GameObject appleProjectilePrefab;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+        if(transform.position.x < -xRange)
+        {
+            transform.position = new Vector3(-xRange, transform.position.y, transform.position.z);
+        }
+
+        if(transform.position.x > xRange)
+        {
+            transform.position = new Vector3(xRange, transform.position.y, transform.position.z);
+        }
+
+
+        horizontalInput = Input.GetAxis("Horizontal");
+        transform.Translate(Vector3.right * horizontalInput * playerSpeed * Time.deltaTime);
+
+        if (Input.GetKeyDown(KeyCode.Space))
+        {
+            //launch projectile from the player
+            Instantiate(appleProjectilePrefab, transform.position, appleProjectilePrefab.transform.rotation);
+        }
+
+
+    }
+}
